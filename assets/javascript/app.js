3cb4b7b92e2c6fb3b7716945b883e3fb1092de15
Andy <Andy@Admins-MacBook-Air.local>
2017-03-20 00:09:16 -0400

    added post guess and end of game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ var unanswered = 0;
 var clock;
 
 //timer which will count down from 30 seconds when each question begins
-var timer = 30;
+var timer = 5;
 
 
 //currentQuestions. this var will match the current index in questionOptions. +1 after each question. this will move to the next question object
@@ -17,19 +17,22 @@ var currentQuestion = 0;
 var question1 = { 
 	question:"What is the first letter of the alphabet?", 
 	answerOptions:[ "a", "b", "c", "d"],
-	answer: 0		
+	answer: "a",
+	answerIndex: 0		
 };
 
 var question2 = { 
 	question:"What is the capital of Georgia?", 
 	answerOptions:[ "Phoenix", "Charlotte", "Atlanta", "Denver"],
-	answer: 2 
+	answer: "Atlanta",
+	answerIndex: 2 
 };
 
 var question3 = { 
 	question:"Who was the first President?", 
 	answerOptions:[ "Nixon", "Washington", "Lincoln", "Garfield"],
-	answer: 1	
+	answer: "Washington",
+	answerIndex: 1	
 };
 
 //question options array- this is where my question objects will be accessed
@@ -45,22 +48,22 @@ questionOptions[2] = question3;
 // console.log(questionOptions[2].question);
 
 
-//this function will begin or reset the game
-function startGame(){
-
-	$(".start").hide(200);
-
-	showQuestion();
-
-};
 
 
 //this function displays the curent timer amount and then deletes one from timer. to be used in an interval.
 function countDown(){
-
+	//display timer value
 	$("#countdown").text(timer);
-
+	//minus one from timer
 	timer--;
+
+	//if timer gets below zero, default wrong answer and add one to unanswered
+	if( timer < 0){
+		postQuestion();	
+		$(".result").text("Sorry, the right answer was " + questionOptions[currentQuestion].answer);
+		unanswered ++;
+	};
+	
 
 };
 
@@ -71,24 +74,102 @@ function showQuestion(){
 
 	console.log(questionOptions[currentQuestion].question);
 
+	//display the current question
 	$("#question").text(questionOptions[currentQuestion].question);
 
+	//display all possible answers
 	for (i = 0; i < questionOptions[currentQuestion].answerOptions.length; i++){
 
-			$("#option"+ i).text(questionOptions[currentQuestion].answerOptions[i]);
+		$("#option"+ i).text(questionOptions[currentQuestion].answerOptions[i]);
 	};
 	
 };
 
 
+//this function will begin the game
+function startGame(){
+
+	//if no more question left, show the game results
+	if (currentQuestion === questionOptions.length){
+		$("#over").show();
+		$("#q-and-a").hide();
+		$("#post-question").hide();
+		$("#correct").text("Correct answers: " + correctAnswers);
+		$("#incorrect").text("Incorrect answers: " + incorrectAnswers);
+		$("#unanswered").text("Unanswered questions: " + unanswered);
+	}
+	else{
+		showQuestion();
+		countDown();
+		//run count down function every second
+		clock = setInterval(countDown, 1000);
+		//shows what you need to see, hides what you dont
+		$("#over").hide();
+		$("#q-and-a").show();
+		$("#post-question").hide();
+};
 
 
+};
 
+//function to start timeout and end interval after the answer is selected or timer gets to zero. adds one to current Question. sets timer back to 30
+function postQuestion(){
+	$("#q-and-a").hide();
+	$("#post-question").show();
+	//stops calling the countdown function every second
+	clearInterval(clock);
+	//add one to currentQuestion- move to next quesiton in the array
+	currentQuestion++;
+	//starts the next question in 3 seconds
+	setTimeout(startGame, 3000);
+	//reset timer
+	timer = 30;
+
+
+}
+
+//start game by clicking start
 $(".start").on("click", function(){
-	showQuestion();
-	countDown();
-	clock = setInterval(countDown, 1000);
+	$(".start").hide();
+	startGame();
 });
+
+//choosing your answer
+$(".answer").on("click", function(){
+
+	//hide question and show the result
+	// $("#q-and-a").hide();
+	// $("#post-question").show();
+
+	//choice equals the value of the clicked div
+	var choice = $(this).attr("value")
+
+	console.log(choice);
+
+	//determine if choice equals the correct answer index or not
+	if(choice == questionOptions[currentQuestion].answerIndex){
+		$(".result").text("THATS RIGHT!");
+		correctAnswers++;
+	}
+	else{
+		$(".result").text("Sorry, the right answer was " + questionOptions[currentQuestion].answer);
+		incorrectAnswers++;
+	};
+
+	//set timer until next question shows, reset clock
+	postQuestion();
+
+});
+
+if (currentQuestion === questionOptions.length){
+
+	$("#correct").text("Correct answers: " + correctAnswers);
+	$("#incorrect").text("Incorrect answers: " + incorrectAnswers);
+	$("#unanswered").text("Unanswered questions: " + unanswered);
+};
+
+//result if clock runs out
+
 
 
 
